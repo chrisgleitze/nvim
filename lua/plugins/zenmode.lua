feeca3d7740a8d5d25e5caf828a69282a9aa12bb
Christian Gleitze <connectingdotscoding@gmail.com>
2024-12-06 13:52:07 +0100

    add zen mode plugin

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/zen-mode.nvim",
+  lazy = false,
+  -- config lent by ThePrimeagen :)
+  config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").setup {
+
+                window = {
+                    width = 100,
+                    options = { }
+                },
+            }
+
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+
+            vim.wo.rnu = true
+        end)
+
+        vim.keymap.set("n", "<leader>zZ", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 80,
+                    options = { }
+                },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = false
+            vim.wo.rnu = false
+        end)
+    end
+}
