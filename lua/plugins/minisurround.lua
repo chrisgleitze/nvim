6637c34c2c24b641f0ae59d17a369ca68b5d39f5
Christian Gleitze <connectingdotscoding@gmail.com>
2024-12-20 13:21:50 +0100

    add reworked mini.surround config, incl. more tpope/vim-surround

--- lua/plugins/minisurround.lua
+++ lua/plugins/minisurround.lua
@@ -4,12 +4,14 @@ return {
   version = false,
   lazy = false,
   keys = function(_, keys)
+    -- Populate the keys based on the user's options
     local opts = LazyVim.opts("mini.surround")
     local mappings = {
       { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
       { opts.mappings.delete, desc = "Delete Surrounding" },
       { opts.mappings.find, desc = "Find Right Surrounding" },
       { opts.mappings.find_left, desc = "Find Left Surrounding" },
+
       { opts.mappings.highlight, desc = "Highlight Surrounding" },
       { opts.mappings.replace, desc = "Replace Surrounding" },
       { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
@@ -20,21 +22,28 @@ return {
     return vim.list_extend(mappings, keys)
   end,
   opts = {
-    custom_surroundings = {
-      ["("] = { output = { left = "( ", right = " )" } },
-      ["["] = { output = { left = "[ ", right = " ]" } },
-      ["{"] = { output = { left = "{ ", right = " }" } },
-      ["<"] = { output = { left = "< ", right = " >" } },
-    },
     mappings = {
-      add = "ys",
-      delete = "ds",
-      find = "",
-      find_left = "",
-      highlight = "",
-      replace = "cs",
-      update_n_lines = "",
+      add = "ys", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      find = "", -- Find surrounding (disabled)
+      find_left = "", -- Find surrounding to the left (disabled)
+      highlight = "", -- Highlight surrounding (disabled)
+      replace = "cs", -- Replace surrounding
+      update_n_lines = "", -- Update `n_lines` (disabled)
+      suffix_last = "", -- Disabled
+
+      suffix_next = "", -- Disabled
     },
     search_method = "cover_or_next",
   },
+  config = function(_, opts)
+    require("mini.surround").setup(opts)
+
+    -- Remap adding surrounding to Visual mode selection
+    vim.keymap.del("x", "ys")
+    vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+    -- Make special mapping for "add surrounding for line"
+    vim.keymap.set("n", "yss", "ys_", { remap = true })
+  end,
 }
