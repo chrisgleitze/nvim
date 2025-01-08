3193bed3989af2b5185fdfb7835ef4971309d9d6
Christian Gleitze <connectingdotscoding@gmail.com>
2025-01-09 00:34:00 +0100

    add oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -14,6 +14,27 @@ return {
     },
   },
 
+  keymaps = {
+    ["<CR>"] = "actions.select",
+
+    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+
+    ["<C-t>"] = { "actions.select", opts = { tab = true } },
+    ["<C-p>"] = "actions.preview",
+    ["<C-c>"] = { "actions.close", mode = "n" },
+    ["<C-l>"] = "actions.refresh",
+    ["-"] = { "actions.parent", mode = "n" },
+    ["_"] = { "actions.open_cwd", mode = "n" },
+    ["`"] = { "actions.cd", mode = "n" },
+
+    ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+    ["gs"] = { "actions.change_sort", mode = "n" },
+    ["gx"] = "actions.open_external",
+    ["g."] = { "actions.toggle_hidden", mode = "n" },
+    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+  },
+
   opts = {
     default_file_explorer = false,
     view_options = {
