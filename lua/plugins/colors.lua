a6a013df1d5e391cb3c9492aec94c583cdfcc68a
Christian Gleitze <connectingdotscoding@gmail.com>
2025-04-10 20:39:38 +0200

    change colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,27 +1,45 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      lazy = false,
-      priority = 1000,
-      undercurl = true,
-      terminalColors = true,
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-    },
-  },
+  -- -- add colorscheme
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   opts = {
+  --     lazy = false,
+  --     priority = 1000,
+  --     undercurl = true,
+  --     terminalColors = true,
+  --     colors = {
+  --       theme = {
+  --         all = {
+  --           ui = {
+  --             bg_gutter = "none",
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+  --
+  -- -- configure LazyVim to load theme
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "kanagawa-dragon",
+  --   },
+  -- },
 
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-dragon",
-    },
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        palettes = {
+          bg1 = "#FF0000",
+        },
+      })
+
+      vim.cmd("colorscheme github_dark_default")
+    end,
   },
 }
